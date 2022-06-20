2b08184fc93df68355c28a802265ecbc27b7d841
Kamigami55 <eason@easonchang.com>
2022-06-20 18:04:46 +0800

    Try add window menu (learn from official guide)
    https://tauri.app/v1/guides/features/menu

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,10 +3,32 @@
   windows_subsystem = "windows"
 )]
 
+use tauri::{CustomMenuItem, Menu, MenuItem, Submenu};
+
 fn main() {
   let context = tauri::generate_context!();
+
+  // here `"quit".to_string()` defines the menu item id, and the second parameter is the menu item label.
+  let quit = CustomMenuItem::new("quit".to_string(), "Quit");
+  let close = CustomMenuItem::new("close".to_string(), "Close");
+  let submenu = Submenu::new("File", Menu::new().add_item(quit).add_item(close));
+  let menu = Menu::new()
+    .add_native_item(MenuItem::Copy)
+    .add_item(CustomMenuItem::new("hide", "Hide"))
+    .add_submenu(submenu);
+
   tauri::Builder::default()
-    .menu(tauri::Menu::os_default(&context.package_info().name))
+    // .menu(tauri::Menu::os_default(&context.package_info().name))
+    .menu(menu)
+    .on_menu_event(|event| match event.menu_item_id() {
+      "quit" => {
+        std::process::exit(0);
+      }
+      "close" => {
+        event.window().close().unwrap();
+      }
+      _ => {}
+    })
     .run(context)
     .expect("error while running tauri application");
 }
