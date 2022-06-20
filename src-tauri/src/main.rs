10e50d40cafd05731d64f7479e642f04ff14d43b
Kamigami55 <eason@easonchang.com>
2022-06-20 19:39:08 +0800

    Try add system tray (learn from official guide)
    https://tauri.app/v1/guides/features/system-tray

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,7 +3,9 @@
   windows_subsystem = "windows"
 )]
 
+use tauri::Manager;
 use tauri::{CustomMenuItem, Menu, MenuItem, Submenu};
+use tauri::{SystemTray, SystemTrayEvent, SystemTrayMenu, SystemTrayMenuItem};
 
 fn main() {
   let context = tauri::generate_context!();
@@ -17,6 +19,15 @@ fn main() {
     .add_item(CustomMenuItem::new("hide", "Hide"))
     .add_submenu(submenu);
 
+  // here `"quit".to_string()` defines the menu item id, and the second parameter is the menu item label.
+  let quit = CustomMenuItem::new("quit".to_string(), "Quit");
+  let hide = CustomMenuItem::new("hide".to_string(), "Hide");
+  let tray_menu = SystemTrayMenu::new()
+    .add_item(quit)
+    .add_native_item(SystemTrayMenuItem::Separator)
+    .add_item(hide);
+  let tray = SystemTray::new().with_menu(tray_menu);
+
   tauri::Builder::default()
     // .menu(tauri::Menu::os_default(&context.package_info().name))
     .menu(menu)
@@ -27,6 +38,41 @@ fn main() {
       "close" => {
         event.window().close().unwrap();
       }
+      _ => {}
+    })
+    .system_tray(tray)
+    .on_system_tray_event(|app, event| match event {
+      SystemTrayEvent::LeftClick {
+        position: _,
+        size: _,
+        ..
+      } => {
+        println!("system tray received a left click");
+      }
+      SystemTrayEvent::RightClick {
+        position: _,
+        size: _,
+        ..
+      } => {
+        println!("system tray received a right click");
+      }
+      SystemTrayEvent::DoubleClick {
+        position: _,
+        size: _,
+        ..
+      } => {
+        println!("system tray received a double click");
+      }
+      SystemTrayEvent::MenuItemClick { id, .. } => match id.as_str() {
+        "quit" => {
+          std::process::exit(0);
+        }
+        "hide" => {
+          let window = app.get_window("main").unwrap();
+          window.hide().unwrap();
+        }
+        _ => {}
+      },
       _ => {}
     })
     .run(context)
